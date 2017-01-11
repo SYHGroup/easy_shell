1690d7f4b268c91e5bf7e327447cac92f397ed66
Jerry <Jerry981028@gmail.com>
2017-01-11 12:47:22 +0800

    Synchronized automatically.

--- shadowsocks/ss-iptables.sh
+++ shadowsocks/ss-iptables.sh
@@ -29,7 +29,7 @@ fi
 }
 function Start(){
 #Get ip from domain
-serverip=`ping ${ssdomain} -s 1 -c 1 | grep ${ssdomain} | head -n 1`
+serverip=`ping ${ssdomain} -s 1 -c 1 -W 2 | grep ${ssdomain} | head -n 1`
 serverip=`echo ${serverip} | cut -d'(' -f 2 | cut -d')' -f1`
 if [ "$serverip" == "" ] ; then
 	echo "错误：查找服务器ip失败，检查网络连接"
