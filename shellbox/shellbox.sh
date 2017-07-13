4328f3b580a4e8b0b6394c16491a0f22c9c67921
Simon Shi <simonsmh@users.noreply.github.com>
2017-07-13 19:27:39 +0800

    refine sysctl

--- shellbox/shellbox.sh
+++ shellbox/shellbox.sh
@@ -71,31 +71,26 @@ deb http://repo.debiancn.org/ stretch main experimental\n' > /etc/apt/sources.li
 
 Setsysctl(){
 Checkroot
-echo -e 'kernel.domainname = simonsmh.cc
-net.core.rmem_max = 134217728
-net.core.wmem_max = 134217728
+echo -e 'kernel.domainname = localhost
+fs.file-max = 65536
 net.core.default_qdisc = fq
-fs.file-max = 51200
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_tw_recycle = 0
-net.ipv4.tcp_fin_timeout = 60
-net.ipv4.tcp_keepalive_time = 3600
-net.ipv4.ip_local_port_range = 10000 65000
-net.ipv4.tcp_max_syn_backlog = 65536
-net.ipv4.tcp_max_tw_buckets = 10000
-net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_mtu_probing=1
-net.ipv4.conf.default.rp_filter=1
-net.ipv4.conf.all.rp_filter=1
-net.ipv4.ip_forward=1
-net.ipv6.conf.all.forwarding=1
-net.ipv6.conf.default.forwarding=1
-net.ipv6.conf.all.accept_ra=2
-net.ipv6.conf.default.accept_ra=2
+net.ipv4.ip_forward = 1
+net.ipv4.ip_local_port_range = 16384 65000
 net.ipv4.tcp_congestion_control = bbr
-net.ipv4.tcp_wmem = 4096 65536 67108864
-net.ipv4.tcp_rmem = 4096 87380 67108864' > /etc/sysctl.conf
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.tcp_max_syn_backlog = 65536
+net.ipv4.tcp_max_tw_buckets = 4096
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_recycle = 0
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1
+net.ipv6.conf.all.forwarding = 1
+net.ipv6.conf.default.forwarding = 1
+net.ipv6.conf.all.accept_ra = 2
+net.ipv6.conf.default.accept_ra = 2\n' > /etc/sysctl.conf
 sysctl -p
 }
 
