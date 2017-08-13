4f56464622357f870be4726100047c7ce84d63e7
Simon Shi <simonsmh@users.noreply.github.com>
2017-08-13 12:57:47 +0800

    Update make.sh

--- module/tcp_tsunami/make.sh
+++ module/tcp_tsunami/make.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-mkdir -p /tmp/tsunami && cd /tmp/tsunami
-wget https://gist.githubusercontent.com/simonsmh/c782f4ff82e492e7493899da73d5cf70/raw/tcp_tsunami.c
-wget https://gist.githubusercontent.com/simonsmh/c782f4ff82e492e7493899da73d5cf70/raw/Makefile
 make && make install
 sed -i 's/bbr/tsunami/g' /etc/sysctl.conf
 sysctl -p
