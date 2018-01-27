e57ce8f9f03dd7025b9a6b6e06688716d2de6af7
Simon Shi <simonsmh@gmail.com>
2018-01-27 17:58:57 +0800

    Update ss.sh

--- useful-commands/ss.sh
+++ useful-commands/ss.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 china_ip_list=/etc/overture/china_ip_list.txt
+config_location=/etc/shadowsocks
+Ignorelist(){
+wget -qO- 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/ignore.list
+[[ "$?" == "0" ]] && mv /tmp/ignore.list $china_ip_list || echo "Failed to update ignore list."
+}
 Checkroot(){
 if [[ $EUID != "0" ]]
 then
@@ -8,18 +13,20 @@ exit 1
 fi
 }
 Help(){
-echo "Usage: $0 <config_name_under_/etc/shadowsocks> <[start]|stop>"
+echo "Usage: $0 <config_name_under_$config_location> <[start]|stop>"
 exit $@
 }
 Checkroot
 [[ -z "$1" ]] && Help 1 || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]] && Help 0 || CONFIG=$1
 [[ "$2" == "stop" ]] && unset ENABLE || ENABLE=start
-ip=$(ping $(sed -n 's/.*"server":"\(.*\)".*/\1/p' /etc/shadowsocks/$CONFIG.json 2>/dev/null) -t1 -c1 -s0 -q 2>/dev/null| sed -n 's/.* (\([0-9.]*\)) .*/\1/p' 2>/dev/null)
-[[ -z "$ip" ]] && {echo "ERROR: Couldn't find your server." && exit 1}
-port=$(sed -n 's/.*"local_port":\([0-9]*\).*/\1/p' /etc/shadowsocks/$CONFIG.json 2>/dev/null)
+if [ $ENABLE ]; then
+[[ $(systemctl is-active shadowsocks-libev-redir@$CONFIG.service) ]] || { echo "ERROR: ss-libev is running." && exit 1;}
+ip=$(ping $(sed -n 's/.*"server":"\(.*\)".*/\1/p' $config_location/$CONFIG.json 2>/dev/null) -t1 -c1 -s0 -q 2>/dev/null| sed -n 's/.* (\([0-9.]*\)) .*/\1/p' 2>/dev/null)
+[[ -z "$ip" ]] && { echo "ERROR: Couldn't find your server." && exit 1;}
+port=$(sed -n 's/.*"local_port":\([0-9]*\).*/\1/p' $config_location/$CONFIG.json 2>/dev/null)
 [[ -z "$port" ]] && port=1080
 # echo $ip $port $ENABLE
-if [ $ENABLE ]; then
+Ignorelist
 systemctl restart shadowsocks-libev-redir@$CONFIG.service
 ss-nat -s $ip -l $port -i $china_ip_list -u -o
 else
