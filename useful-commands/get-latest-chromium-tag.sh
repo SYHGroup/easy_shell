25e0c6607d395a498cebd80a6dff9598f3f9aa02
Jerry <isjerryxiao@outlook.com>
2019-10-11 23:58:55 +0800

    get-latest-chromium-tag.sh: produce fake ua

--- useful-commands/get-latest-chromium-tag.sh
+++ useful-commands/get-latest-chromium-tag.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl -s 'https://api.github.com/repos/chromium/chromium/tags' | grep -F '"name":' | sed -n 's/[ \t]*"name":[ ][ ]*"\(.*\)".*/\1/p' | grep -E '([0-9]+.){3}[0-9]+' | head -n 1
+chromium_tag=$(curl -s 'https://api.github.com/repos/chromium/chromium/tags' | grep -F '"name":' | sed -n 's/[ \t]*"name":[ ][ ]*"\(.*\)".*/\1/p' | grep -E '([0-9]+.){3}[0-9]+' | head -n 1)
+
+echo "$chromium_tag" >&2
+
+echo "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${chromium_tag} Safari/537.36"
